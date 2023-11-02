82df2b77a4cd58786004d78849867d63a2515ff8
Elias Escalante <144855358+eliasescalante@users.noreply.github.com>
2023-11-01 21:05:51 -0300

    arreglo- conversor de docx a pdf

--- conversor_de_docx_a_pdf.py
+++ conversor_de_docx_a_pdf.py
@@ -2,7 +2,7 @@ from docx2pdf import convert
 
 # Ruta del archivo de Word a convertir
 print(" Conversor de doc a pdf...")
-input_docx_file = input("ingresa la ruta del archivo docx a convertir : ")
+documento_a_convertir = input("ingresa la ruta del archivo docx a convertir : ")
 
 # Convierte el archivo de Word a PDF
-convert(input_docx_file)
+convert(documento_a_convertir)
